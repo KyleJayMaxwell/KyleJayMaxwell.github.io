4b52814a3d1066036ab70adbfaa8abacad08d2dd
Kyle Maxwell <kylejaymaxwell@gmail.com>
2015-10-26 22:20:25 -0600

    IG api.

--- js/variables.js
+++ js/variables.js
@@ -1,5 +1,3 @@
-$(document).foundation();
-
 $.ajax({
   url: 'https://api.instagram.com/v1/users/1721552028/media/recent?access_token=1721552028.5b9e1e6.1ef4cc17d83a4c14990feaffc31b5b90',
   type: 'GET',
@@ -10,12 +8,6 @@ $.ajax({
     alert('An error has occurred');
   },
   success: function(results){
-  	for(i=0;i<states.length;i++){
-  		console.log(results.data[i]);
-  		var post = results.data[i];
-  		var text = 
-  		$('#instagram ul').append("<li>"+post+"<p>"+"</li>");
-
+  console.log(results.data);
     }
-  }
 });
