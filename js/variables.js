55542f09bc617d7c2849a20ae7cf5371d86527b9
Kyle Maxwell <kylejaymaxwell@gmail.com>
2015-11-02 19:25:06 -0700

    IG api

--- js/variables.js
+++ js/variables.js
@@ -8,6 +8,20 @@ $.ajax({
     alert('An error has occurred');
   },
   success: function(results){
-  console.log(results.data);
+    for(i=0;i<results.data.length;i++){
+      console.log(results.data[i]);
+      if(i%2){      
+        var post2 = results.data[i].images.standard_resolution.url;
+        var text2 = results.data[i].caption.text;
+        var limit2 = text2.substring(0, 35);
+        var link2 = results.data[i].link;
+        $('#instagram2 ul').append("<li><img src=\""+post2+"\" /><p>"+limit2+"... <a class=\"iglink\" href=\"link2\">See the Picture</a></p></li>");
+      } else{
+        var post = results.data[i].images.standard_resolution.url;
+        var text = results.data[i].caption.text;
+        var limit = text.substring(0, 35);
+        var link = results.data[i].link;
+        $('#instagram ul').append("<li><img src=\""+post+"\" /><p>"+limit+"... <a class=\"iglink\" href=\"link\">See the Picture</a></p></li>");      }
     }
+  }
 });
