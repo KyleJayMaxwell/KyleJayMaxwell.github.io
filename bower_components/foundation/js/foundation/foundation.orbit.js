cd5d836d55044a374ad1cd19699e6316da24c9ff
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-01-16 23:27:28 -0700

    clean up of some code, fixed nav bar, and slider, renamed

--- bower_components/foundation/js/foundation/foundation.orbit.js
+++ bower_components/foundation/js/foundation/foundation.orbit.js
@@ -407,11 +407,11 @@
   Foundation.libs.orbit = {
     name : 'orbit',
 
-    version : '5.5.2',
+    version : '5.5.3',
 
     settings : {
       animation : 'slide',
-      timer_speed : 10000,
+      timer_speed : 7000,
       pause_on_hover : true,
       resume_on_mouseout : false,
       next_on_click : true,
